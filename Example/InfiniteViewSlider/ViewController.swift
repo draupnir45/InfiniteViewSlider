84e880b85ce9b6e5fa877d7a914e785f1ac8f66e
Jongchan Park <draupnir45@gmail.com>
2018-04-23 17:49:12 +0900

    Update podspec, screenshot & example. (Now using AutoLayout for label positioning.)

--- Example/InfiniteViewSlider/ViewController.swift
+++ Example/InfiniteViewSlider/ViewController.swift
@@ -16,50 +16,61 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    // 1st View
+    // View Items
     let redView = UIView()
-    redView.backgroundColor = .red
     let redViewLabel = UILabel()
-    redViewLabel.text = "Hi, swipe me!"
-    redViewLabel.textColor = .white
-    redViewLabel.font = UIFont.boldSystemFont(ofSize: 36.0)
     redView.addSubview(redViewLabel)
-    redViewLabel.sizeToFit()
-    redViewLabel.center = CGPoint(x: slider.bounds.width / 2.0, y: slider.bounds.height / 2.0)
-
-    // 2nd View
     let blueView = UIView()
-    blueView.backgroundColor = .blue
     let blueViewLabel = UILabel()
-    blueViewLabel.text = "You can also enable \nauto slide!"
-    blueViewLabel.numberOfLines = 0
-    blueViewLabel.textColor = .white
-    blueViewLabel.textAlignment = .center
-    blueViewLabel.font = UIFont.boldSystemFont(ofSize: 36.0)
     blueView.addSubview(blueViewLabel)
-    blueViewLabel.sizeToFit()
-    blueViewLabel.center = CGPoint(x: slider.bounds.width / 2.0, y: slider.bounds.height / 2.0)
-
-    // 3rd View
     let dogsImageView = UIImageView(image: #imageLiteral(resourceName: "dogs.jpg"))
-    dogsImageView.contentMode = .scaleAspectFill
 
     // Setting slider
-    slider.viewArray = [redView, blueView] // Set UIView Array to `viewArray`
-    slider.slideTimeInterval = 1.0 // default is 3.0.
-    slider.viewArray.append(dogsImageView) // You can add views later
+    slider.slidingSubviews = [redView, blueView, dogsImageView] // Set UIView Array to `viewArray`
+    slider.autoSlideTimeInterval = 1.0 // default is 3.0.
     slider.delegate = self
+    print(redView.frame)
 
+    // 1st View
+    redView.backgroundColor = .red
+    redViewLabel.text = "Hi, swipe me!"
+    redViewLabel.textColor = .white
+    redViewLabel.textAlignment = .center
+    redViewLabel.font = UIFont.boldSystemFont(ofSize: 36.0)
+
+    // Label AutoLayout
+    redViewLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      NSLayoutConstraint(item: redViewLabel, attribute: .centerX, relatedBy: .equal, toItem: redView, attribute: .centerX, multiplier: 1.0, constant: 0.0),
+      NSLayoutConstraint(item: redViewLabel, attribute: .centerY, relatedBy: .equal, toItem: redView, attribute: .centerY, multiplier: 1.0, constant: 0.0)
+                                 ])
+
+    // 2nd View
+    blueView.backgroundColor = .blue
+    blueViewLabel.text = "You can also\nenable auto slide!"
+    blueViewLabel.font = UIFont.boldSystemFont(ofSize: 36.0)
+    blueViewLabel.textColor = .white
+    blueViewLabel.textAlignment = .center
+    blueViewLabel.numberOfLines = 0
+
+    // Label AutoLayout
+    blueViewLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      NSLayoutConstraint(item: blueViewLabel, attribute: .centerX, relatedBy: .equal, toItem: blueView, attribute: .centerX, multiplier: 1.0, constant: 0.0),
+      NSLayoutConstraint(item: blueViewLabel, attribute: .centerY, relatedBy: .equal, toItem: blueView, attribute: .centerY, multiplier: 1.0, constant: 0.0)
+      ])
+
+    // 3rd View
+    dogsImageView.contentMode = .scaleAspectFill
   }
 
   @IBAction func toggleAutoSlide(_ sender: UISwitch) {
     slider.isAutoSlideEnabled = sender.isOn
   }
-
 }
 
 extension ViewController: InfiniteViewSliderDelegate {
-  func sliderDidSlide(_ slider: InfiniteViewSlider, contentOffset: Double, numberOfViews: Int, currentViewIndex: Int) {
-    print(contentOffset, numberOfViews, currentViewIndex)
+  func sliderDidSlide(_ slider: InfiniteViewSlider, progress: Double, numberOfViews: Int, currentViewIndex: Int) {
+    print(progress, numberOfViews, currentViewIndex)
   }
 }
